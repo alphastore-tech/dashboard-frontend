eb07f92017aebf836802e4bc9311ec443f152dee
antaechan <0627antaechan@gmail.com>
2025-05-20 21:51:23 +0900

    Fix: next config compiler field

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    tsconfigPaths: true,
+    compiler: {
+        // tsconfig.json의 "paths" 항목을 webpack-alias로 그대로 적용
+        tsconfigPaths: true,
+      },
 };
 module.exports = nextConfig;
