11110258bd45b468c28bcad55a2a8d837dd35f58
antaechan <0627antaechan@gmail.com>
2025-08-22 19:19:30 +0900

    Remove: useless code

--- hooks/usePeriodPnl.ts
+++ hooks/usePeriodPnl.ts
@@ -12,43 +12,14 @@ const fetcher = (url: string) =>
       throw err;
     });
 
-
 export function useDailyPeriodPnl() {
   // 두 날짜가 모두 들어와야 요청
-  const key =`${process.env.NEXT_PUBLIC_BACKEND_URL}/pnl/daily`;
-  return useSWR(key, fetcher, {
-    
-  });
+  const key = `${process.env.NEXT_PUBLIC_BACKEND_URL}/pnl/daily`;
+  return useSWR(key, fetcher, {});
 }
 
 export function useMonthlyPeriodPnl() {
   // 두 날짜가 모두 들어와야 요청
-  const key =`${process.env.NEXT_PUBLIC_BACKEND_URL}/pnl/monthly`;
-  return useSWR(key, fetcher, {
-    
-  });
+  const key = `${process.env.NEXT_PUBLIC_BACKEND_URL}/pnl/monthly`;
+  return useSWR(key, fetcher, {});
 }
-
-
-
-
-
-// /**
-//  * 기간별 PnL 훅
-//  *
-//  * @param startDate  조회 시작일 (YYYYMMDD)
-//  * @param endDate    조회 종료일 (YYYYMMDD)
-//  */
-// export default function usePeriodPnl(startDate: string | undefined, endDate: string | undefined) {
-//   // 두 날짜가 모두 들어와야 요청
-//   const key =
-//     startDate && endDate ? `/api/period-pnl?startDate=${startDate}&endDate=${endDate}` : null;
-
-//   const marketOpen = isMarketOpenKST(900, 1545);
-
-//   return useSWR(key, fetcher, {
-//     refreshInterval: marketOpen ? 600_000 : 0, // 600 000 ms = 10 min
-//     dedupingInterval: 550_000,
-//     keepPreviousData: true,
-//   });
-// }
